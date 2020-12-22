d761df0f3472fb64a6377689016177c1d3d4a23e
Vlad Zamaev <vlad@MacBook-Pro-Vlad.local>
2020-12-22 13:09:06 +0300

    Started dice

---
+++ Dice/Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Vlad Zamaev on 22.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
