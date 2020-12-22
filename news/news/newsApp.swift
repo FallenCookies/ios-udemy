f17de82f0658b317c5b7f568c6a139a2b2bb1cf5
Vlad Zamaev <vlad@MacBook-Pro-Vlad.local>
2020-12-22 20:14:15 +0300

    make news

---
+++ news/news/newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp.swift
+//  news
+//
+//  Created by Vlad Zamaev on 22.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
